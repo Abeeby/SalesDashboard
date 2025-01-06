8b6a26fc34c9c75c97efb9df2fa9fa6bb64eec34
Abeeby <amin.torrisi@hotmail.com>
2025-01-06 16:52:58 +0100

    feat: Ajout du thème et amélioration de la navbar
    - Implémentation du mode sombre/clair avec ThemeContext
    - Ajout du menu responsive pour mobile
    - Système de notifications dans la navbar
    - Nouveau design avec gradient et animations
    - Support des thèmes Material-UI personnalisés

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Navbar from './components/Navbar';
+import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
