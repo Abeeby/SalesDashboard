cdabae7d8da226ca4a1b98c8ee8ccf3cc087a2ae
Abeeby <amin.torrisi@hotmail.com>
2025-01-06 18:21:14 +0100

    feat: Ajout des utilitaires de gestion des cookies
    - Création des fonctions de gestion des cookies
    - Support du domaine personnalisé
    - Fonctions pour définir, obtenir et supprimer les cookies
    - Ajout d'une fonction pour nettoyer tous les cookies

---
+++ src/utils/cookies.js
@@ -0,0 +1,27 @@
+export const setCookie = (name, value, domain = 'dashboradsales.web.app', days = 30) => {
+  const date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = `expires=${date.toUTCString()}`;
+  document.cookie = `${name}=${value};${expires};domain=${domain};path=/;secure;samesite=strict`;
+};
+
+export const getCookie = (name) => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+  return null;
+};
+
+export const deleteCookie = (name, domain = 'dashboradsales.web.app') => {
+  document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;domain=${domain};path=/`;
+};
+
+export const clearAllCookies = () => {
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i];
+    const eqPos = cookie.indexOf('=');
+    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
+  }
+}; 
