7c1b0d5a1bc79a07ead62ece07e62c7d9b8b70ba
Abeeby <amin.torrisi@hotmail.com>
2025-01-08 15:38:48 +0100

    fix: Suppression des imports inutilisés
    - Suppression de BarChart, Bar et Cell de recharts
    - Nettoyage du code pour passer les vérifications ESLint

--- src/components/VintedDashboard.js
+++ src/components/VintedDashboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {
   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
-  BarChart, Bar, PieChart, Pie, Cell
+  PieChart, Pie
 } from 'recharts';
 import CircularProgress from '@mui/material/CircularProgress';
 import Alert from '@mui/material/Alert';
